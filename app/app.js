b506747aae0ae4d25ab2a530d67acd1937131f2a
Chen Gao <maxlin2905@gmail.com>
2020-09-15 22:59:13 +1000

    tues

---
+++ app/app.js
@@ -0,0 +1,70 @@
+var myApp = angular.module('myApp',['ngRoute'])
+
+myApp.config(['$routeProvider',function($routeProvider){
+
+    $routeProvider
+    .when('/home',{
+        templateUrl:'view/home.html', 
+        controller:'myController'
+    })
+    .when('/directory',{
+        templateUrl:'view/directory.html',
+        controller:'myController'
+    }).otherwise(
+        {
+            redirectTo:'/directory'
+        }
+    )
+}])
+myApp.directive('randomHero',[function(){
+  
+  
+    return {
+        restrict:'E',
+        scope:{
+            heros:'=',
+            title:'='
+        },
+        templateUrl:'view/random.html',
+        controller:function($scope){
+
+            $scope.random=Math.floor(Math.random() * 5) 
+        }
+
+    }
+
+}])
+myApp.controller('myController',['$scope','$http',function($scope,$http){
+console.log('in the controller')
+$scope.message='oh, mighty God! I am praying to you!'
+
+//console.log(angular.toJson($scope.heros))
+$scope.order ='name'
+$scope.removeHero =function(hero){
+    console.log('removing')
+   // $scope.heros.filter(item=>item.name!=hero.name)
+  const removedIndex = $scope.heros.indexOf(hero)
+  $scope.heros.splice(removedIndex,1)
+}
+
+$scope.addNewHero=function(){
+    $scope.heros.push({
+        name:$scope.newHero.name,
+        power:$scope.newHero.power,
+        cost:parseInt($scope.newHero.cost),
+        available:true
+    })
+    $scope.clearValues()
+
+}
+
+$scope.clearValues=function(){
+    $scope.newHero.name =''
+    $scope.newHero.power=''
+    $scope.newHero.cost =''
+}
+$http.get('data/heros.json')
+.then((response)=>response.data)
+.then(data=>$scope.heros=data)
+
+}])
